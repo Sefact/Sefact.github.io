4aaba1bfb2a714a3a17170ec8961c9f33d2c4435
Sefact <lime17442@gmail.com>
2022-08-11 01:52:36 +0900

    init vitepress custom

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,12 +1,8 @@
 import Layout from './Layout.vue'
+import DefaultTheme from 'vitepress/theme'
 export default {
-    // root component to wrap each page
-    Layout,
-    // this is a Vue 3 functional component
-    NotFound: () => 'custom 404',
-    enhanceApp({ app, router, siteData }) {
-        // app is the Vue 3 app instance from `createApp()`.
-        // router is VitePress' custom router. `siteData` is
-        // a `ref` of current site-level metadata.
+    ...DefaultTheme,
+    enhanceApp({ app }) {
+        // register global components
     }
 }
