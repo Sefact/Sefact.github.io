aa250868a1384af1cabe8e809225599a1ef928f1
Sefact <lime17442@gmail.com>
2022-12-19 23:36:00 +0900

    sidebar

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,7 +7,17 @@ export default defineConfig({
 
     lastUpdated: true,
 
-    // themeConfig: {
-    //
-    // }
+    themeConfig: {
+        nav: [
+            { text: 'Guide', link: '/guide' },
+            {
+                text: 'Dropdown Menu',
+                items: [
+                    { text: 'Item A', link: '/item-1' },
+                    { text: 'Item B', link: '/item-2' },
+                    { text: 'Item C', link: '/item-3' }
+                ]
+            }
+        ]
+    }
 })
