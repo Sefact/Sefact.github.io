3a1fef84b31970cc8d6561f2217d94152641c4a0
Sefact <lime17442@gmail.com>
2023-02-05 16:54:06 +0900

    sidebar collapsed

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -22,6 +22,7 @@ export default defineConfig({
         sidebar: [
             {
                 text: 'Section Title A',
+                collapsed: true,
                 items: [
                     { text: 'Item A', link: '/item-a' },
                     { text: 'Item B', link: '/item-b' },
@@ -29,6 +30,7 @@ export default defineConfig({
             },
             {
                 text: 'Section Title B',
+                collapsed: true,
                 items: [
                     { text: 'Item C', link: '/item-c' },
                     { text: 'Item D', link: '/item-d' },
