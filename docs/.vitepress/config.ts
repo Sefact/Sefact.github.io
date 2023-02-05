15e0e08e3d05cc882a4802f8daf689dd302f302e
Sefact <lime17442@gmail.com>
2023-02-05 16:39:21 +0900

    sidebar

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -18,6 +18,22 @@ export default defineConfig({
                     { text: 'Item C', link: '/item-3' }
                 ]
             }
+        ],
+        sidebar: [
+            {
+                text: 'Section Title A',
+                items: [
+                    { text: 'Item A', link: '/item-a' },
+                    { text: 'Item B', link: '/item-b' },
+                ]
+            },
+            {
+                text: 'Section Title B',
+                items: [
+                    { text: 'Item C', link: '/item-c' },
+                    { text: 'Item D', link: '/item-d' },
+                ]
+            }
         ]
     }
 })
