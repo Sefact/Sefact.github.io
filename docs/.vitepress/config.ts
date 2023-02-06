a71dc70834871652aa94ed51e8805d68b14177cc
Sefact <lime17442@gmail.com>
2023-02-06 22:15:40 +0900

    Google Analytics 추가

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitepress'
+import {defineConfig} from 'vitepress'
 
 export default defineConfig({
     lang: 'en-US',
@@ -7,15 +7,26 @@ export default defineConfig({
 
     lastUpdated: true,
 
+    head: [
+        [
+            'script',
+            {
+                async: 'true',
+                src: "https://www.googletagmanager.com/gtag/js?id=G-BFXZNR3013",
+            },
+        ],
+        ['script', {}, 'window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag("js", new Date()); gtag("config", "G-BFXZNR3013");'],
+    ],
+
     themeConfig: {
         nav: [
-            { text: 'Guide', link: '/guide' },
+            {text: 'Guide', link: '/guide'},
             {
                 text: 'Dropdown Menu',
                 items: [
-                    { text: 'Item A', link: '/item-1' },
-                    { text: 'Item B', link: '/item-2' },
-                    { text: 'Item C', link: '/item-3' }
+                    {text: 'Item A', link: '/item-1'},
+                    {text: 'Item B', link: '/item-2'},
+                    {text: 'Item C', link: '/item-3'}
                 ]
             }
         ],
@@ -24,16 +35,16 @@ export default defineConfig({
                 text: 'Section Title A',
                 collapsed: false,
                 items: [
-                    { text: 'Item A', link: '/item-a' },
-                    { text: 'Item B', link: '/item-b' },
+                    {text: 'Item A', link: '/item-a'},
+                    {text: 'Item B', link: '/item-b'},
                 ]
             },
             {
                 text: 'Section Title B',
                 collapsed: false,
                 items: [
-                    { text: 'Item C', link: '/item-c' },
-                    { text: 'Item D', link: '/item-d' },
+                    {text: 'Item C', link: '/item-c'},
+                    {text: 'Item D', link: '/item-d'},
                 ]
             }
         ]
