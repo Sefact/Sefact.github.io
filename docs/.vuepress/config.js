0bfeb0afbe1133deabdf1510f466399100566626
Sefact <lime17442@gmail.com>
2023-02-07 22:39:09 +0900

    Vitepress > Vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: "Vuepress",
+    description: "Description",
+    themeConfig: {
+        nav: [
+            { text: "Home", link: "/" },
+            { text: "About", link: "/about/" },
+        ]
+    }
+};
