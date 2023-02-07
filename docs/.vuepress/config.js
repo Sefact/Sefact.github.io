0dfe4a463fb4f72742177879210104a91a3d92e7
Sefact <lime17442@gmail.com>
2023-02-07 22:47:35 +0900

    Google Analytics 추가

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,16 @@
 module.exports = {
     title: "Vuepress",
     description: "Description",
+    head: [
+        [
+            "script",
+            {
+                async: true,
+                src: "https://www.googletagmanager.com/gtag/js?id=G-BFXZNR3013",
+            },
+        ],
+        ["script", {}, ["window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-BFXZNR3013');"]],
+    ],
     themeConfig: {
         nav: [
             { text: "Home", link: "/" },
