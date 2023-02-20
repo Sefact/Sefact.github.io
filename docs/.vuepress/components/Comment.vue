3bc8dbd13afb39c3583955440fdd5e0a33d7f1f4
Sefact <lime17442@gmail.com>
2023-02-20 23:28:54 +0900

    utterances 추가

---
+++ docs/.vuepress/components/Comment.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+
+export default{
+  name: "Comment",
+  components: {},
+  methods: {
+    initValine() {
+      let utterances = window.document.createElement('script')
+      utterances.type = 'text/javascript'
+      utterances.src = 'https://utteranc.es/client.js'
+      utterances.async = true
+      utterances.setAttribute('issue-term', 'pathname')
+      utterances.setAttribute('theme', 'github-light')
+      utterances.setAttribute('repo', 'Sefact/Sefact.github.io')
+      utterances.crossorigin = 'anonymous'
+
+      window.document.getElementById('comment').appendChild(utterances)
+    }
+  },
+  mounted() {
+    this.initValine()
+  }
+};
+</script>
+
+<template>
+  <div id="comment"></div>
+</template>
+
+<style scoped>
+
+</style>
