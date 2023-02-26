2de34871a4a0e46090cb8fb7d8a13ec202f1944b
Sefact <lime17442@gmail.com>
2023-02-26 17:51:43 +0900

    cwebp 문서 작성중

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,33 +1,46 @@
 module.exports = {
-    title: "Vuepress",
-    description: "Description",
-    head: [
-        [
-            "script",
-            {
-                async: true,
-                src: "https://www.googletagmanager.com/gtag/js?id=G-BFXZNR3013",
-            },
-        ],
-        ["script", {}, ["window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-BFXZNR3013');"]],
+  title: "Vuepress",
+  description: "Description",
+  head: [
+    [
+      "script",
+      {
+        async: true,
+        src: "https://www.googletagmanager.com/gtag/js?id=G-BFXZNR3013",
+      },
     ],
-    themeConfig: {
-        nav: [
-            { text: "Home", link: "/" },
-            { text: "About", link: "/about/" },
-        ],
-        sidebar: [
-            {
-                title: 'Group 1',
-                children: [ '/' ],
-                collapsable: true,
-            },
-            {
-                title: 'Group 2',
-                children: [ '/' ],
-                collapsable: true,
-            }
-        ]
+    [
+      "script",
+      {},
+      [
+        "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-BFXZNR3013');",
+      ],
+    ],
+  ],
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "About", link: "/about/" },
+    ],
+    sidebar: [
+      {
+        title: "Group 1",
+        children: ["/guide/cwebp"],
+        collapsable: true,
+      },
+      {
+        title: "Group 2",
+        children: ["/"],
+        collapsable: true,
+      },
+    ],
+  },
+  plugins: [["sitemap", { hostname: "https://sefact.github.io/" }]],
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@guide": "/docs/guide",
+      },
     },
-    plugins: [["sitemap", { hostname: "https://sefact.github.io/" }]]
+  },
 };
